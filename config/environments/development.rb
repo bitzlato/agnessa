b3d66b58105e36bf3e163937050e91af437c498a
Danil Pismenny <danil@brandymint.ru>
2022-03-29 13:42:31 +0300

    Use regexp to whiny hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.hosts << ENV['AGNESSA_HOST'] if ENV.key? 'AGNESSA_HOST'
+  config.hosts << /.*\.?#{ENV.fetch('AGNESSA_HOST')}/ if ENV.key? 'AGNESSA_HOST'
   # Show full error reports.
   config.consider_all_requests_local = true
 
