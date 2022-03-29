05f2d9e48ea26ec30c4476c62704bf383d304353
Alexander Logunov <unlovedru@gmail.com>
2022-03-29 14:00:36 +0300

    [#181624793] verification callback check

--- app/assets/javascript/admin.js
+++ app/assets/javascript/admin.js
@@ -11,9 +11,23 @@
 //= require_tree ./elements
 //= require_tree ./extra
 
-// document.addEventListener("turbolinks:load", function() {
+document.addEventListener("turbolinks:load", function() {
+  const verification_callback_form = document.querySelector("#verification_callback_form");
+  const verification_callback_form_result = document.querySelector("#verification_callback_result");
+
+  verification_callback_form.addEventListener("ajax:beforeSend", (event) => {
+    verification_callback_form_result.innerHTML = '<div class="spinner-border"><span class="sr-only">Loading...</span></div>';
+  });
+  verification_callback_form.addEventListener("ajax:success", (event) => {
+    const [_data, _status, xhr] = event.detail;
+    verification_callback_form_result.innerHTML = xhr.responseText;
+  });
+  verification_callback_form.addEventListener("ajax:error", () => {
+    verification_callback_form_result.innerHTML = "<p>ERROR</p>";
+  });
+  
 //   $('[data-toggle="tooltip"]').tooltip()
 //   $('.datetimepicker').datetimepicker({
 //      format: 'YYYY-MM-DD HH:mm'
 //   })
-// })
+})
