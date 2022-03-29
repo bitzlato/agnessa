0f4036e331a9388ac9f022afdc2be6243c285ceb
Danil Pismenny <danil@brandymint.ru>
2022-03-29 15:37:28 +0300

    Remove app/controllers/api

--- app/controllers/legacy_verifications_controller.rb
+++ app/controllers/legacy_verifications_controller.rb
@@ -1,6 +1,18 @@
-class LegacyVerificationsController < API::APIController
+class LegacyVerificationsController < ApplicationController
   def index
-    @verifications = Verification.all.order('created_at DESC')
-    render json: @verifications.map { |x| x.legacy_show}
+    verifications = Verification.
+      all.
+      order('created_at DESC').
+      map do |v|
+      {
+        id: v.legacy_verification_id,
+        status: v.status.to_s == 'confirmed' ? true : false,
+        comment: v.raw_changebot['comment'],
+        time: v.legacy_created || (v.created_at.to_i * 1000)
+
+      }
+    end
+
+    render json: verifications
   end
 end
