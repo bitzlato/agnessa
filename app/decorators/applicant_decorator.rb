7ff09a6a4d61c8089b6a60376bc626904bf4e5a1
elnikov <elnikov.s@gmail.com>
2022-03-30 13:05:56 +0300

    [#181717668] add fields

--- app/decorators/applicant_decorator.rb
+++ app/decorators/applicant_decorator.rb
@@ -10,6 +10,24 @@ class ApplicantDecorator < ApplicationDecorator
   end
 
   def self.table_columns
-    %i[id account_id external_id created_at blocked]
+    %i[id account_id applicant_name external_id created_at last_confirmed_verification_id blocked]
+  end
+
+  def applicant_name
+    "#{last_verification.name} #{last_verification.last_name}"
+  end
+
+  def last_confirmed_verification_id
+    last_verification.id
+  end
+
+  def confirmed_at
+    last_verification.confirmed_at
+  end
+
+  private
+
+  def last_verification
+    @last_verification ||= object.verifications.last
   end
 end
