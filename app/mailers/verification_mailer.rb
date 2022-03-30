937f2bbf0c28375807b19c6c7c927adaf1e24628
elnikov <elnikov.s@gmail.com>
2022-03-30 08:01:29 +0300

    [#181605136] add client_from

--- app/mailers/verification_mailer.rb
+++ app/mailers/verification_mailer.rb
@@ -1,11 +1,15 @@
 class VerificationMailer < ApplicationMailer
   def confirmed(verification_id)
     @verification = Verification.find(verification_id)
-    mail to: @verification.email, subject: 'Вы успешно прошли верификацию'
+    @applicant = @verification.applicant
+    @client = @applicant.client
+    mail to: @verification.email, rom: @client.email_from, subject: 'Вы успешно прошли верификацию'
   end
 
   def refused(verification_id)
     @verification = Verification.find(verification_id)
-    mail to: @verification.email, subject: 'Вы не прошли верификацию'
+    @applicant = @verification.applicant
+    @client = @applicant.client
+    mail to: @verification.email, from: @client.email_from, subject: 'Вы не прошли верификацию'
   end
 end
