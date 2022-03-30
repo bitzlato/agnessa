71d76a2af5a9b3b80caa75d2dbdf21f679bdb311
elnikov <elnikov.s@gmail.com>
2022-03-30 17:43:13 +0300

    [#181717668] fix

--- spec/factories/applicant.rb
+++ spec/factories/applicant.rb
@@ -2,8 +2,11 @@ FactoryBot.define do
   factory :applicant do
     association :account
     sequence(:external_id) { |i| "external_id_#{i}" }
-    first_name { Faker::Name.name }
-    last_name { Faker::Name.name }
-    confirmed_at { Time.now }
+
+    trait :confirmed do
+      first_name { Faker::Name.name }
+      last_name { Faker::Name.name }
+      confirmed_at { Time.now }
+    end
   end
 end
