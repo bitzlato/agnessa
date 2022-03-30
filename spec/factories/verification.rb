456cb936b3b51bddbe2a8d450a4646c469e319ad
Danil Pismenny <danil@brandymint.ru>
2022-03-30 10:06:22 +0300

    Fix specs

--- spec/factories/verification.rb
+++ spec/factories/verification.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     sequence(:last_name) { |i| "LastName_#{i}" }
     sequence(:passport_data) { |i| "passport_#{i}" }
     sequence(:legacy_verification_id) { |i| "legacy_id_#{i}" }
-    sequence(:reason) { |i| Verification.reasons.keys.sample }
+    sequence(:reason) { |i| Verification::REASONS.sample }
     sequence(:email) { |i| "email#{i}@domain.com" }
 
     documents { [Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/image.jpg')))] }
