84ce7c4a0b71de0ba091efa283aa06d3283678a4
Danil Pismenny <danil@brandymint.ru>
2022-03-30 09:18:58 +0300

    hotfix landing

--- app/controllers/public/landing_controller.rb
+++ app/controllers/public/landing_controller.rb
@@ -2,6 +2,6 @@ class Public::LandingController < Public::ApplicationController
   layout 'simple'
 
   def index
-    render locals: { account: Account.all }
+    render locals: { accounts: Account.all }
   end
 end
