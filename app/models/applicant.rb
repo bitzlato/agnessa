eba6453d14f99e81674a182a1ce736007d397a00
Alexander Logunov <unlovedru@gmail.com>
2022-03-31 10:44:47 +0300

    [#181625157] журналирование заявок и аппликантов

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,6 +1,7 @@
 class Applicant < ApplicationRecord
   belongs_to :account
   has_many :verifications
+  has_many :log_records
   belongs_to :last_confirmed_verification, class_name: 'Verification', required: false
 
   validates :last_confirmed_verification_id, :first_name, :last_name, presence: true, if: :confirmed_at?
@@ -13,5 +14,19 @@ class Applicant < ApplicationRecord
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def block! member=nil
+    ActiveRecord::Base.transaction do
+      update!(blocked: true)
+      log_records.create!(action: 'block', member: member)
+    end
+  end
+
+  def unblock! member=nil
+    ActiveRecord::Base.transaction do
+      update!(blocked: false)
+      log_records.create!(action: 'unblock', member: member)
+    end
   end
 end
