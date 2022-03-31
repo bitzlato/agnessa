e4c77836d7bb5dbcd2bf6af4ab8083f774c348bc
elnikov <elnikov.s@gmail.com>
2022-03-31 13:12:40 +0300

    [#181717668] update status on applicant (#22)
    * [#181717668] add fields to applicant
    
    * [#181717668] add search form
    
    * [#181717668] fix
    
    * [#181717668] add belongs to
    
    * [#181717668] move full name
    
    * [#181717668] fix
    
    * [#181717668] add transaction
    
    * [#181717668] validate if confirmed

--- spec/factories/applicant.rb
+++ spec/factories/applicant.rb
@@ -2,5 +2,11 @@ FactoryBot.define do
   factory :applicant do
     association :account
     sequence(:external_id) { |i| "external_id_#{i}" }
+
+    trait :confirmed do
+      first_name { Faker::Name.name }
+      last_name { Faker::Name.name }
+      confirmed_at { Time.now }
+    end
   end
 end
