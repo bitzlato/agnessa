4a2c5de4ebb3ae145af5058f1d39cd335883458f
elnikov <elnikov.s@gmail.com>
2022-03-31 10:47:43 +0300

    [#181717668] validate if confirmed

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,8 +1,9 @@
 class Applicant < ApplicationRecord
   belongs_to :account
   has_many :verifications
-  belongs_to :last_confirmed_verification, class_name: 'Verification'
+  belongs_to :last_confirmed_verification, class_name: 'Verification', required: false
 
+  validates :last_confirmed_verification_id, :first_name, :last_name, presence: true, if: :confirmed_at?
   validates :external_id, presence: true, uniqueness: {scope: :account}
 
   def to_s
