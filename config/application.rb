a877a83ecff76939dc7734126aaafb74d89ea058
elnikov <elnikov.s@gmail.com>
2022-04-01 15:32:20 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module Agnessa
       "#{Rails.root}/app/services",
       "#{Rails.root}/app/uploaders",
       "#{Rails.root}/app/decorators",
-      "#{Rails.root}/lib",
+      "#{Rails.root}/app/lib",
     ]
 
   end
