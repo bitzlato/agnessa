22a7c23cdc331e18588fee19316e9393b455c073
elnikov <elnikov.s@gmail.com>
2022-04-04 11:11:48 +0300

    Update account.rb

--- app/models/account.rb
+++ app/models/account.rb
@@ -28,8 +28,6 @@ class Account < ApplicationRecord
 
   def form_description_interpolated
     Interpolation.interpolate form_description, interpolate_hash
-  rescue
-    ''
   end
 
   def interpolate_hash
