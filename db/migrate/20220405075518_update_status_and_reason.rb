7d73054a31e9dc4bcc7ebee39d401305aae1462d
elnikov <elnikov.s@gmail.com>
2022-04-05 11:00:27 +0300

    fix

--- db/migrate/20220405075518_update_status_and_reason.rb
+++ db/migrate/20220405075518_update_status_and_reason.rb
@@ -3,7 +3,6 @@ class UpdateStatusAndReason < ActiveRecord::Migration[6.1]
     Verification.where("raw_changebot->>'status' = ?", 'refused').update_all status: 'refused'
     Verification.where("raw_changebot->>'status' = ?", 'confirmed').update_all status: 'confirmed'
     Verification.where("raw_changebot->>'status' = ?", 'new').update_all status: 'pending'
-    Verification.where("raw_changebot->>'status' = ?", 'new').update_all status: 'pending'
 
     Verification.where("raw_changebot->>'cause' = ?", 'unlocking').update_all status: 'unban'
     Verification.where("raw_changebot->>'cause' = ?", 'restoring').update_all status: 'restore'
