db892509b81993e448fd941ca990d632146b1d67
elnikov <elnikov.s@gmail.com>
2022-04-05 09:34:38 +0300

    duplicate fields in atteribures

--- app/decorators/verification_decorator.rb
+++ app/decorators/verification_decorator.rb
@@ -6,7 +6,7 @@ class VerificationDecorator < ApplicationDecorator
   end
 
   def self.attributes
-    table_columns + %i[legacy_verification_id applicant external_id]
+    %i[id created_at country status reason name_dup last_name_dup email_dup document_number_dup moderator legacy_verification_id applicant external_id]
   end
 
   def self.table_columns
@@ -26,6 +26,25 @@ class VerificationDecorator < ApplicationDecorator
                          'restore' => 'badge badge-info',
                          'other' => 'badge badge-mute', }
 
+  DUPLICATE_CLASSES = { true => 'badge badge-warning',
+                        false => 'badge badge-info', }
+
+  def document_number_dup
+    duplicate_field(dup_documents, object.document_number, h.admin_verifications_path('q[document_number_eq]': object.last_name))
+  end
+
+  def name_dup
+    duplicate_field(dup_names, object.name, h.admin_verifications_path('q[name_eq]': object.last_name))
+  end
+
+  def last_name_dup
+    duplicate_field(dup_names, object.last_name, h.admin_verifications_path('q[last_name_eq]': object.last_name))
+  end
+
+  def email_dup
+    duplicate_field(dup_emails, object.email, h.admin_verifications_path('q[email_eq]': object.email))
+  end
+
   def external_id
     h.content_tag(:code, object.applicant.external_id)
   end
@@ -38,4 +57,25 @@ class VerificationDecorator < ApplicationDecorator
     h.content_tag(:span, I18n.t(object.reason, scope: :reason), class: CSS_REASON_CLASSES[object.reason])
   end
 
+  private
+
+  def duplicate_field(scope, field, path)
+    if scope.count > 1
+      h.link_to h.content_tag(:span, field, class: DUPLICATE_CLASSES[true]), path, target: '_blank'
+    else
+      field
+    end
+  end
+
+  def dup_documents
+    @dup_documents ||= Verification.where(document_number: verification.document_number)
+  end
+
+  def dup_names
+    @dup_names ||= Verification.where(name: verification.name).where(last_name: verification.last_name)
+  end
+
+  def dup_emails
+    @πdup_emails ||= Verification.where(email: verification.email)
+  end
 end
