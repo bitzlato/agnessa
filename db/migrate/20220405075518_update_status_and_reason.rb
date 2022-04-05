ceaf45cd00681f32f69b668ff3fc855ea50cec4d
elnikov <elnikov.s@gmail.com>
2022-04-05 12:27:14 +0300

    fix

--- db/migrate/20220405075518_update_status_and_reason.rb
+++ db/migrate/20220405075518_update_status_and_reason.rb
@@ -4,9 +4,9 @@ class UpdateStatusAndReason < ActiveRecord::Migration[6.1]
     Verification.where("raw_changebot->>'status' = ?", 'confirmed').update_all status: 'confirmed'
     Verification.where("raw_changebot->>'status' = ?", 'new').update_all status: 'pending'
 
-    Verification.where("raw_changebot->>'cause' = ?", 'unlocking').update_all status: 'unban'
-    Verification.where("raw_changebot->>'cause' = ?", 'restoring').update_all status: 'restore'
-    Verification.where("raw_changebot->>'cause' = ?", 'other').update_all status: 'other'
-    Verification.where("raw_changebot->>'cause' = ?", 'trusted_trader').update_all status: 'trusted_trader'
+    Verification.where("raw_changebot->>'cause' = ?", 'unlocking').update_all reason: 'unban'
+    Verification.where("raw_changebot->>'cause' = ?", 'restoring').update_all reason: 'restore'
+    Verification.where("raw_changebot->>'cause' = ?", 'other').update_all reason: 'other'
+    Verification.where("raw_changebot->>'cause' = ?", 'trusted_trader').update_all reason: 'trusted_trader'
   end
 end
