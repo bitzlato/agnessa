d6f445b3a241a2c790b160c09b595214721bcd85
Danil Pismenny <danil@brandymint.ru>
2022-04-07 18:30:37 +0300

    Add gallery

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -5,6 +5,18 @@ class DocumentUploader < CarrierWave::Uploader::Base
     Rails.configuration.application.min_upload_file_size..Rails.configuration.application.max_upload_file_size
   end
 
+  def extension
+    file.extension.downcase
+  end
+
+  def mime_type
+    MIME::Types.type_for(file).first.content_type
+  end
+
+  def video?
+    mime_type.include? 'video'
+  end
+
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
