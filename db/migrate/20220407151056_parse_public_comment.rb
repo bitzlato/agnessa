0cbf3b2bf353580b2a7ba5ee89a6d298612890b6
elnikov <elnikov.s@gmail.com>
2022-04-07 19:23:49 +0300

    Parse public comment from mongo (#52)
    * add public and private comments
    
    * add public and private comments

---
+++ db/migrate/20220407151056_parse_public_comment.rb
@@ -0,0 +1,10 @@
+class ParsePublicComment < ActiveRecord::Migration[6.1]
+  def change
+    Verification.all.find_each do |v|
+      comment = v.raw_changebot['comment']
+      next if comment.blank?
+
+      v.update_columns public_comment: comment
+    end
+  end
+end
