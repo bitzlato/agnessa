cdc08dbcbb501d57a683af07bf8513dc9cfa6a54
Danil Pismenny <dapi@users.noreply.github.com>
2022-04-07 18:02:40 +0300

    Update 20220407141738_add_emails_to_applicant.rb

--- db/migrate/20220407141738_add_emails_to_applicant.rb
+++ db/migrate/20220407141738_add_emails_to_applicant.rb
@@ -1,5 +1,5 @@
 class AddEmailsToApplicant < ActiveRecord::Migration[6.1]
   def change
-    add_column :applicants, :emails, :jsonb, default: []
+    add_column :applicants, :emails, :jsonb, default: [], null: false
   end
 end
