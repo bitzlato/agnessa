a4f6aaa1068626619d0c139b82e4c6f2835b66bb
elnikov <elnikov.s@gmail.com>
2022-04-08 12:47:14 +0300

    upd

--- app/models/mongo/verification.rb
+++ app/models/mongo/verification.rb
@@ -27,44 +27,47 @@ class Mongo::Verification
     field '__v', type: Integer
 
 
-    # Mongo::Verification.all.no_timeout.map{ |verification| verification.import_to_postgres }
-    def import_to_postgres
-        return if ::Verification.find_by(legacy_verification_id:  self['_id'])
+    # Mongo::Verification.all.order('c')no_timeout.map{ |verification| verification.import_to_postgres }
+    def self.import_to_postgres
+        Mongo::Verification.all.no_timeout.map do |mongo_verification|
+            pg_verifcation = ::Verification.find_or_initialize_by legacy_verification_id: mongo_verification['_id']
+            next if pg_verifcation.status != 'pending'
 
-        pg_verifcation = ::Verification.new legacy_verification_id: self['_id']
-        account = Account.first
-        applicant = account.applicants.find_or_create_by!(external_id: pg_verifcation.legacy_verification_id)
-        pg_verifcation.applicant_id = applicant.id
-        if self.status == 'new'
-            pg_verifcation.status = 'pending'
-        else
-            pg_verifcation.status = status
-        end
-        pg_verifcation.raw_changebot = self
-        pg_verifcation.documents = []
-        pg_verifcation.public_comment = self.comment
-        emails = Array(emails).map(&:downcase).compact.uniq
-        pg_verifcation.email = emails.last
-        applicant.emails = emails
-        raw = pg_verifcation.raw_changebot
+            account = Account.first
+            applicant = account.applicants.find_or_create_by!(external_id: pg_verifcation.legacy_verification_id)
+            pg_verifcation.applicant_id = applicant.id
+            if mongo_verification.status == 'new'
+                pg_verifcation.status = 'pending'
+            else
+                pg_verifcation.status = mongo_verification.status
+            end
+            pg_verifcation.raw_changebot = mongo_verification
+            pg_verifcation.documents = []
+            pg_verifcation.public_comment = mongo_verification.comment
+            emails = Array(emails).map(&:downcase).compact.uniq
+            pg_verifcation.email = emails.last
+            applicant.emails = emails
 
-        case raw['cause']
-        when 'trusted'
-            pg_verifcation.reason = 'trusted_trader'
-        when 'other'
-            pg_verifcation.reason = 'other'
-        when 'unlocking'
-            pg_verifcation.reason = 'unban'
-        when 'restoring'
-            pg_verifcation.reason = 'restore'
+            raw = pg_verifcation.raw_changebot
+
+            case raw['cause']
+            when 'trusted'
+                pg_verifcation.reason = 'trusted_trader'
+            when 'other'
+                pg_verifcation.reason = 'other'
+            when 'unlocking'
+                pg_verifcation.reason = 'unban'
+            when 'restoring'
+                pg_verifcation.reason = 'restore'
+            end
+            pg_verifcation.document_number = raw['passportData']
+            pg_verifcation.name = raw['name']
+            pg_verifcation.last_name = raw['lastName']
+            pg_verifcation.created_at = raw['created']
+            pg_verifcation.updated_at = raw['lastUpdate']
+            pg_verifcation.comment = raw['comment']
+            pg_verifcation.save(validate: false)
+            applicant.save(validate: false)
         end
-        pg_verifcation.document_number = raw['passportData']
-        pg_verifcation.name = raw['name']
-        pg_verifcation.last_name = raw['lastName']
-        pg_verifcation.created_at = raw['created']
-        pg_verifcation.updated_at = raw['lastUpdate']
-        pg_verifcation.comment = raw['comment']
-        pg_verifcation.save(validate: false)
-        applicant.save(validate: false)
     end
 end
