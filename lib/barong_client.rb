8a7b4ec0f48b88bc1f22cea82e9acadf9f5a97a5
Danil Pismenny <danil@brandymint.ru>
2022-04-11 17:49:58 +0300

    Separate root_url for BarongClient

--- lib/barong_client.rb
+++ lib/barong_client.rb
@@ -1,15 +1,25 @@
 class BarongClient
+  include Singleton
+
   OPEN_TIMEOUT = 10 # opening connection timeout in seconds
   TIMEOUT = 300 # waiting for response timeout in seconds
 
+  def initialize(api_root_url: ENV.fetch('AGNESSA_BARONG_API_ROOT_URL'))
+    @uri = URI api_root_url
+  end
+
   def get_uid_from_changebot_id id
-    response = client.get("/api/v2/barong/public/user_uid_by_changebot_id/#{id}")
+    response = client.get(uri.path + "/public/user_uid_by_changebot_id/#{id}")
     response.assert_success!
     response.body['uid']
   end
 
+  private
+
+  attr_reader :uri
+
   def client
-    Faraday.new(ENV.fetch('AGNESSA_BARONG_API_ROOT_URL')) do |conn|
+    Faraday.new uri do |conn|
       conn.response :json
       conn.adapter Faraday.default_adapter
       conn.options.open_timeout = OPEN_TIMEOUT
