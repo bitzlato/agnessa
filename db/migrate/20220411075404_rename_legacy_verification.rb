4be831d09646ff0b48cc2e15d63c0d12a6d03688
elnikov <elnikov.s@gmail.com>
2022-04-11 11:20:13 +0300

    fix

--- db/migrate/20220411075404_rename_legacy_verification.rb
+++ db/migrate/20220411075404_rename_legacy_verification.rb
@@ -1,6 +1,6 @@
 class RenameLegacyVerification < ActiveRecord::Migration[6.1]
   def change
-    rename_column :verifications, :legacy_external_id, :legacy_external_id
+    rename_column :verifications, :legacy_verification_id, :legacy_external_id
     add_column :verifications, :legacy_id, :string
   end
 end
