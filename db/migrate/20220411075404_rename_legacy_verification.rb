1e28f942442cc13451a5e0eb22023bfef59323be
elnikov <elnikov.s@gmail.com>
2022-04-11 12:02:36 +0300

    remove legacy id

--- db/migrate/20220411075404_rename_legacy_verification.rb
+++ db/migrate/20220411075404_rename_legacy_verification.rb
@@ -1,6 +1,5 @@
 class RenameLegacyVerification < ActiveRecord::Migration[6.1]
   def change
     rename_column :verifications, :legacy_verification_id, :legacy_external_id
-    add_column :verifications, :legacy_id, :string
   end
 end
