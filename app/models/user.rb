637db196a2c9f9d2f463276259b3c0df6b7ce189
Danil Pismenny <danil@brandymint.ru>
2022-04-12 13:11:36 +0300

    m

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  has_many :members
+  has_many :accounts, through: :members
+
   validates :email, presence: true
   validates :email, uniqueness: true
 
