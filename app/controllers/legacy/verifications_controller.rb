10a8d5381be3a360b493b143b9dc8c0016803a2e
Alexander Logunov <unlovedru@gmail.com>
2022-04-13 12:43:05 +0300

    support of verification old external_id

--- app/controllers/legacy/verifications_controller.rb
+++ app/controllers/legacy/verifications_controller.rb
@@ -23,15 +23,13 @@ class Legacy::VerificationsController < ApplicationController
   end
 
   def show
-    uid = BarongClient.instance.get_uid_from_changebot_id(params[:id])
-    account = Account.first
-    if uid.present? and account.present?
-      encoded_external_id = VerificationUrlGenerator.generate_token(uid, account.secret)
+    account = Account.find_by_subdomain('bz')
+    if account.present?
+      encoded_external_id = VerificationUrlGenerator.generate_token(params[:id], account.secret)
       url_options = Rails.configuration.application.default_url_options.symbolize_keys
       url_options[:subdomain] = account.subdomain
       redirect_to new_client_verification_url(encoded_external_id, url_options)
     else
-      Bugsnag.notify(StandardError.new("Unknown Changebot Id: #{params[:id]}"))
       render :error
     end
   end
