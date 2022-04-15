2074e7be247cb19ece5446c84754387f2a943e8a
elnikov <elnikov.s@gmail.com>
2022-04-15 22:02:31 +0300

    back js

--- app/assets/javascript/refuse_append.js
+++ app/assets/javascript/refuse_append.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
-
-
+document.addEventListener("turbolinks:load", function() {
     addLabelsToPublicComment = function () {
         var selected = $(`#verification_review_result_labels option:selected`);
 
@@ -12,10 +10,7 @@ $(document).ready(function() {
         });
     }
 
-
-    $(`#verification_review_result_labels`).change(function() {
-        addLabelsToPublicComment()
-    })
+    $('#verification_review_result_labels').change(addLabelsToPublicComment)
 });
 
 
