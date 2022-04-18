592828d196ba3b39d7fcbff97196f6a65398f08b
elnikov <elnikov.s@gmail.com>
2022-04-18 16:19:06 +0300

    hide applicant comment

--- app/assets/javascript/verification.js
+++ app/assets/javascript/verification.js
@@ -2,6 +2,22 @@
 //= require simple_form-datetimepicker
 
 $(document).ready(function() {
+
+    toggleVisibleApplicantComment = function () {
+        var applicantComment = $("#applicant-comment");
+        var selected = $(`#verification_reason`).val();
+
+        if (selected == 'restore') {
+            applicantComment.show();
+
+        } else {
+            applicantComment.hide();
+        }
+    }
+
+    toggleVisibleApplicantComment();
+
+    $('#verification_reason').change(toggleVisibleApplicantComment);
 });
 
 
