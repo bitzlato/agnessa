3eeb64e983df8a9178be3e1ff076fb1b2e14526b
elnikov <elnikov.s@gmail.com>
2022-04-18 18:44:35 +0300

    Toggle visible applicant comment (#89)
    * hide applicant comment
    
    * fix PR
    
    * Update new.html.slim
    
    * fix
    
    * Update verification.js
    
    * fix
    
    * fix
    
    * fix

--- app/assets/javascript/verification.js
+++ app/assets/javascript/verification.js
@@ -1,7 +1,18 @@
 //= require moment
 //= require simple_form-datetimepicker
 
-$(document).ready(function() {
+const toggleVisibleApplicantComment = function () {
+    var applicantComment = $("[data-applicant-comment]");
+
+    if (this.value == 'restore') {
+        applicantComment.show();
+    } else {
+        applicantComment.hide();
+    }
+}
+$( document ).ready(function() {
+    toggleVisibleApplicantComment();
+    $('[data-verification-reason]').change(toggleVisibleApplicantComment);
 });
 
 
