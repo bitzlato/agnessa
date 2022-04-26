f54b6c714976b7c7aac4479c5c0ad8560cfcc57e
elnikov <elnikov.s@gmail.com>
2022-04-26 13:12:43 +0300

    add archive methid to controller

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -2,7 +2,7 @@ class Admin::MembersController < Admin::ApplicationController
   # before_action :superadmin?
 
   def index
-    members = paginate current_account.members.active
+    members = paginate current_account.members.alive
     render locals: {members: members}
   end
 
@@ -33,6 +33,11 @@ class Admin::MembersController < Admin::ApplicationController
   rescue ActiveRecord::RecordInvalid => e
     raise e unless e.record.is_a? Member
     render :edit, locals: {member: member}
+  end
+
+  def archive
+    member.archive!
+    redirect_to admin_members_url, notice: 'Admin user was successfully archived.'
   end
 
   def destroy
