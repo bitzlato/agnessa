352b8c9e0670c69e3336a36082e5a0791d7f01bb
elnikov <elnikov.s@gmail.com>
2022-05-04 18:36:43 +0300

    public comment

--- app/services/refuse_empty_verification.rb
+++ app/services/refuse_empty_verification.rb
@@ -1,12 +1,13 @@
 class RefuseEmptyVerification
   def refuse_empty(moderator: nil)
     member = Member.find(3) if member.nil?
+    private_comment = 'Автоматически отклоненно. Пустые имя, фамилия, пасспортные данные.'
+    public_comment = 'Фотографии плохого качества'
 
     Verification.confirmed.where("document_number = '' OR last_name = '' OR name = ''").find_each do |verification|
       verification.reason = 'other' if verification.reason.nil?
 
-      comment = 'Автоматически отклоненно. Пустые имя, фамилия, пасспортные данные.'
-      verification.refuse! member: member, public_comment: comment, private_comment: comment, labels: ['BAD_PHOTO'], send_email: false
+      verification.refuse! member: member, public_comment: public_comment, private_comment: private_comment, labels: ['BAD_PHOTO'], send_email: false
     end
   end
 end
