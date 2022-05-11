3eadefd3ce53bb2aa5168e9d137fd3d22313e0bd
elnikov <elnikov.s@gmail.com>
2022-05-11 12:08:25 +0300

    add presentatino to errors

--- app/controllers/concerns/rescue_errors.rb
+++ app/controllers/concerns/rescue_errors.rb
@@ -5,12 +5,16 @@
 module RescueErrors
   extend ActiveSupport::Concern
 
+  class UnauthorizedMember < StandardError; end;
+  class ArchivedMember < StandardError; end;
+
   included do
     # rescue_from ActionController::UnknownFormat,     with: :rescue_unknown_format
     # rescue_from Authority::SecurityViolation,        with: :rescue_security_violation
     # rescue_from NotAuthenticated,                    with: :not_authenticated
     # rescue_from ActionController::InvalidAuthenticityToken, with: :rescue_invalid_authenticity_token
 
+    rescue_from UnauthorizedMember, ArchivedMember,  with: :rescue_security_violation
     rescue_from ActionController::MissingFile,       with: :not_found
     rescue_from ActiveRecord::RecordNotFound,        with: :not_found
     rescue_from HumanizedError,                      with: :handle_humanized_error
