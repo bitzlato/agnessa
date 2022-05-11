980e2e1b13113b39e985f4537546a829e0ae4134
elnikov <elnikov.s@gmail.com>
2022-05-11 10:09:43 +0300

    change syntax

--- db/migrate/20220511065127_set_reason_when_it_null.rb
+++ db/migrate/20220511065127_set_reason_when_it_null.rb
@@ -1,7 +1,7 @@
 class SetReasonWhenItNull < ActiveRecord::Migration[6.1]
   def change
     Verification.confirmed.where(reason: nil).find_each do |verification|
-      verification.update reason: 'other', updated_at: verification.updated_at if verification.reason.nil?
+      verification.update_column('reason', 'other') if verification.reason.nil?
     end
   end
 end
