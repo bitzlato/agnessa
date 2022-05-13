cf7ac0d235ccbffdd303503db2cf03c2f08af845
elnikov <elnikov.s@gmail.com>
2022-05-13 11:23:13 +0300

    required true

--- db/migrate/20220513073228_add_number_to_verification.rb
+++ db/migrate/20220513073228_add_number_to_verification.rb
@@ -1,8 +1,9 @@
 class AddNumberToVerification < ActiveRecord::Migration[6.1]
   def change
     add_column :verifications, :number, :string
-    Verification.all.find_each do |v|
-      v.update_column('number', v.id)
-    end
+    Verification.update_all 'number=cast(id)'
+    # Verification.all.find_each do |v|
+    #   v.update_column('number', v.id)
+    # end
   end
 end
