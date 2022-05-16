2d182458db8858e1811f640f29a8c3e7f441f8f2
Danil Pismenny <danil@brandymint.ru>
2022-05-16 12:15:59 +0300

    Use enum for members role

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,32 +1,25 @@
 class Member < ApplicationRecord
   STATISTICS_PERIODS = [1.day, 1.month, 6.months]
+  ROLES = %w[operator admin].freeze
 
   include Archivable
 
   belongs_to :account
   belongs_to :user
 
+  has_many :verifications, class_name: 'Verification', foreign_key: 'moderator_id'
+
   validates :account, :user, presence: true
   validates :user, uniqueness: {scope: :account}
 
-  ROLES = %w[operator admin].freeze
   validates :role, presence: true, inclusion: { in: ROLES }
-
-  def operator?
-    role == 'role'
-  end
-
-  def admin?
-    role == 'admin'
-  end
-
-  has_many :verifications, class_name: 'Verification', foreign_key: 'moderator_id'
+  enum role: ROLES.each_with_object({}) { |e,a| a[e] = e }
 
   def label
     to_s
   end
 
   def to_s
-    user.email
+    user.to_s
   end
 end
