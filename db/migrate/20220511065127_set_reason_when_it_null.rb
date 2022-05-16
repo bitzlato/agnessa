31056fdec47ea9fd958334e4591f0d4e61cd9a44
elnikov <elnikov.s@gmail.com>
2022-05-16 12:43:33 +0300

    migrate to task

--- db/migrate/20220511065127_set_reason_when_it_null.rb
+++
@@ -1,8 +0,0 @@
-class SetReasonWhenItNull < ActiveRecord::Migration[6.1]
-  def change
-    return unless Rails.env.production?
-    Verification.confirmed.where(reason: nil).find_each do |verification|
-      verification.update_column('reason', 'other') if verification.reason.nil?
-    end
-  end
-end
