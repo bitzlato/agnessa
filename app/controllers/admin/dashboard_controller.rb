ccd4eb483e058a3d1c749c2f8aaa66e6823eb87b
elnikov <elnikov.s@gmail.com>
2022-05-18 12:03:00 +0300

    Add reason for dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,3 +1,10 @@
 class Admin::DashboardController < Admin::ApplicationController
+  include RansackSupport
   skip_before_action :admin_member, only: [:index]
+
+  private
+
+  def model_class
+    Verification
+  end
 end
