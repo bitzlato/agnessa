3757b4682456a1629d2449545374849f4b9575c9
Danil Pismenny <dapi@users.noreply.github.com>
2022-05-19 11:27:19 +0300

    Update 20220518074223_add_index_to_number.rb

--- db/migrate/20220518074223_add_index_to_number.rb
+++ db/migrate/20220518074223_add_index_to_number.rb
@@ -1,5 +1,5 @@
 class AddIndexToNumber < ActiveRecord::Migration[6.1]
   def change
-    add_index(:verifications, :number)
+    add_index :verifications, [:application_id, :number], unique: true
   end
 end
