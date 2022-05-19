72b7a5422fe0e27a55e283cfc556b86976a35d1f
elnikov <elnikov.s@gmail.com>
2022-05-19 11:21:44 +0300

    [#182194397] log records  (#115)
    * Add controller
    
    * Add routes
    
    * Add decorator
    
    * Add index
    
    * Add search
    
    * Add navbar
    
    * add i18n
    
    * remove from dashboard
    
    * Update log_records_controller.rb
    
    * fix
    
    * spec index 200
    
    * only index
    
    * fix i18n

---
+++ app/decorators/log_record_decorator.rb
@@ -0,0 +1,19 @@
+class LogRecordDecorator < ApplicationDecorator
+  delegate_all
+
+  def self.attributes
+    table_columns
+  end
+
+  def self.table_columns
+    %i[action verification full_name document_number member created_at]
+  end
+
+  def full_name
+    object.verification.full_name
+  end
+
+  def document_number
+    object.verification.document_number
+  end
+end
