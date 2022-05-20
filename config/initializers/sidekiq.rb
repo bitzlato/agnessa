e165db8546adf1bb61915f694f1df6b4c5372657
Alexander Logunov <unlovedru@gmail.com>
2022-05-20 18:46:21 +0300

    sidekiq with namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,11 +4,11 @@ require 'sidekiq'
 require 'sidekiq/web'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('AGNESSA_REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = { url: ENV.fetch('AGNESSA_REDIS_URL', 'redis://localhost:6379/0'), namespace: "agnessa_#{Rails.env}" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('AGNESSA_REDIS_URL', 'redis://localhost:6379/0') }
+  config.redis = { url: ENV.fetch('AGNESSA_REDIS_URL', 'redis://localhost:6379/0'), namespace: "agnessa_#{Rails.env}" }
 end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
