866f2cda578c188f5d4bdfd818b02ee943f203e7
Danil Pismenny <dapi@users.noreply.github.com>
2022-05-24 10:36:25 +0300

    Staged cookie name (#120)

--- config/initializers/session.rb
+++ config/initializers/session.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+key = '_agnessa'
+key += '_' + Rails.env unless Rails.env.production?
 Rails.application.config.session_store :cookie_store,
-                                        key: '_agnessa',
+                                        key: key,
                                         domain: :all,
                                         tld_length:  Rails.configuration.application.default_url_options.fetch(:host).split('.').count
