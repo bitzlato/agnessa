e4ac506c52deb18849a0fd027379d080cb3a6b6e
elnikov <elnikov.s@gmail.com>
2022-05-26 13:01:41 +0300

    Partial for fields

--- app/models/document_type.rb
+++ app/models/document_type.rb
@@ -4,4 +4,7 @@ class DocumentType < ApplicationRecord
   FILE_TYPES = %w[video image].freeze
   enum file_type: FILE_TYPES.each_with_object({}) { |e, a| a[e] = e }
 
+  # validates :account_id, uniqueness: {scope: [:verification_id, ]}
+
+
 end
