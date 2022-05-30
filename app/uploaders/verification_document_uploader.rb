37eaa0d64d22fb1628cb77ff693c855ae9372545
elnikov <elnikov.s@gmail.com>
2022-05-30 20:30:14 +0300

    fix

--- app/uploaders/verification_document_uploader.rb
+++ app/uploaders/verification_document_uploader.rb
@@ -42,7 +42,6 @@ class VerificationDocumentUploader < CarrierWave::Uploader::Base
   end
 
   def content_type_allowlist
-    # Rails.configuration.application.document_content_types
     model.content_types
   end
 
