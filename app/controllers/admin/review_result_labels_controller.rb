a341f1a0ba6976280e9a940e747fab433d738a69
elnikov <elnikov.s@gmail.com>
2022-05-30 10:31:19 +0300

    Add edit

--- app/controllers/admin/review_result_labels_controller.rb
+++ app/controllers/admin/review_result_labels_controller.rb
@@ -6,8 +6,24 @@ class Admin::ReviewResultLabelsController < Admin::ApplicationController
   end
 
   def edit
+    render locals: {label: label}
   end
 
   def update
+    label.update!(label_params)
+    redirect_back fallback_location: admin_review_result_labels_path, notice: 'Label was successfully updated.'
+  rescue ActiveRecord::RecordInvalid => e
+    raise e unless e.record.is_a? Label
+    render :edit, locals: {label: label}
+  end
+
+  private
+
+  def label
+    @label ||= ReviewResultLabel.find(params[:id])
+  end
+
+  def label_params
+    params.require(:review_result_label).permit(:label_ru, :label, :public_comment)
   end
 end
