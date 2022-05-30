e7792cf7c22f743b86c10a94b2dcc9e14e1ec218
elnikov <elnikov.s@gmail.com>
2022-05-30 10:46:11 +0300

    Feature/[#182306098] edit label (#126)
    * Add index
    
    * Add edit
    
    * Move to form

--- app/controllers/admin/review_result_labels_controller.rb
+++ app/controllers/admin/review_result_labels_controller.rb
@@ -1,2 +1,29 @@
-class Admin::ReviewResultLabelsController < Admin::ResourcesController
+class Admin::ReviewResultLabelsController < Admin::ApplicationController
+
+  def index
+    labels = paginate ReviewResultLabel.all.order('created_at DESC')
+    render locals: {labels: labels}
+  end
+
+  def edit
+    render locals: {label: label}
+  end
+
+  def update
+    label.update!(label_params)
+    redirect_back fallback_location: admin_review_result_labels_path, notice: 'Label was successfully updated.'
+  rescue ActiveRecord::RecordInvalid => e
+    raise e unless e.record.is_a? Label
+    render :edit, locals: {label: label}
+  end
+
+  private
+
+  def label
+    @label ||= ReviewResultLabel.find(params[:id])
+  end
+
+  def label_params
+    params.require(:review_result_label).permit(:label_ru, :label, :public_comment)
+  end
 end
