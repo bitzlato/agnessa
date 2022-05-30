058e857c8dbd5e1546e60f3e050c7e718e4f7cd7
elnikov <elnikov.s@gmail.com>
2022-05-30 09:13:24 +0300

    Verification document permits

--- app/models/verification_document.rb
+++ app/models/verification_document.rb
@@ -1,8 +1,8 @@
 class VerificationDocument < ApplicationRecord
   mount_uploader :file, VerificationDocumentUploader
 
-  belongs_to :verification
-  belongs_to :document_type
+  belongs_to :verification, required: true
+  belongs_to :document_type, required: true
 
   delegate :content_types, to: :document_type
 end
