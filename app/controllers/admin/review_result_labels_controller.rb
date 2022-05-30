3b51da1caab32ce1e6b2b4623921711e33ff3d56
elnikov <elnikov.s@gmail.com>
2022-05-30 10:14:50 +0300

    Add index

--- app/controllers/admin/review_result_labels_controller.rb
+++ app/controllers/admin/review_result_labels_controller.rb
@@ -1,2 +1,13 @@
-class Admin::ReviewResultLabelsController < Admin::ResourcesController
+class Admin::ReviewResultLabelsController < Admin::ApplicationController
+
+  def index
+    labels = paginate ReviewResultLabel.all.order('created_at DESC')
+    render locals: {labels: labels}
+  end
+
+  def edit
+  end
+
+  def update
+  end
 end
