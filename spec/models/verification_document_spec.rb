a34b15ce1cb8bb38de8f031dfe52959d6ab7798e
elnikov <elnikov.s@gmail.com>
2022-05-30 19:55:07 +0300

    Spec

--- spec/models/verification_document_spec.rb
+++ spec/models/verification_document_spec.rb
@@ -5,9 +5,6 @@ RSpec.describe VerificationDocument, type: :model do
   let(:video_document) { create(:verification_document, :video) }
 
   describe :create do
-    it 'success create video verification document' do
-      expect(video_document.id).to be_truthy
-    end
     it 'success create image verification document' do
       expect(image_document.id).to be_truthy
     end
