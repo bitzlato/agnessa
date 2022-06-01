3fca5b272a4432cffc967dce20bc3efae931e12b
elnikov <elnikov.s@gmail.com>
2022-06-01 12:00:35 +0300

    spec

--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Country, type: :model do
   let(:country) { create(:country, :full_types)}
-  pending "add some examples to (or delete) #{__FILE__}"
 
   describe :create do
     it 'success' do
