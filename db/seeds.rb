d6e5e055a0d4cb3a298ba9be1853a862c2ee6ca9
elnikov <elnikov.s@gmail.com>
2022-06-16 15:04:03 +0300

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,15 +14,23 @@ account = Account.
 
 user = User.create_with(password: 'test').find_or_create_by!(email: 'test@test.test')
 
-account.members.create_with(role: 'superadmin').
+account.members.create_with(role: 'admin').
   find_or_create_by!(user: user)
 
 applicant = account.applicants.find_or_create_by!(external_id: 'test')
 
-applicant.verifications.create!({
-  name: 'test', last_name: 'test', legacy_external_id: 'test', country: 'ru', document_number: 'test',
-  reason: :unban, email: 'test@test.test', status: :pending,
-  documents: [Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/image.jpg')), "image/jpeg")]
+verification = applicant.verifications.build({
+  name: 'test', last_name: 'test', birth_date: '01-01-1991', gender: :male, legacy_external_id: 'test', country: 'ru', document_number: 'test',
+  reason: :unban, email: 'test@test.test', status: :pending
 })
+
+account.document_types.available.each do |document_type|
+  if document_type.file_type == 'video'
+    verification.verification_documents.new document_type: document_type, file: File.open(File.join(Rails.root, '/spec/fixtures/video.mp4'))
+  else
+    verification.verification_documents.new document_type: document_type, file: File.open(File.join(Rails.root, '/spec/fixtures/image.jpg'))
+  end
+end
+verification.save!
 
 Agnessa::Seed.new.seed_review_result_labels
