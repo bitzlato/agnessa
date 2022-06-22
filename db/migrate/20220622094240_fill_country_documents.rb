820d87e3d0da24a7854208c65793ebe29f27083f
elnikov <elnikov.s@gmail.com>
2022-06-22 17:36:55 +0300

    form

--- db/migrate/20220622094240_fill_country_documents.rb
+++
@@ -1,5 +0,0 @@
-class FillCountryDocuments < ActiveRecord::Migration[6.1]
-  def change
-    Country.update_all(available_documents: Rails.application.config.application.available_documents)
-  end
-end
