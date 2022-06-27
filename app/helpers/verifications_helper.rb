efec59861f591137b4bf43789f4a4f48105aa9f4
elnikov <elnikov.s@gmail.com>
2022-06-27 08:44:36 +0300

    fix: Refactor

--- app/helpers/verifications_helper.rb
+++ app/helpers/verifications_helper.rb
@@ -6,7 +6,7 @@ module VerificationsHelper
     status_from_query.blank? ? scope : scope.by_status(status_from_query)
   end
 
-  def tmp_path(path)
-    "/#{CarrierWave::Uploader::Base.cache_dir}/#{path}"
+  def tmp_full_path(relative_path)
+    "/#{CarrierWave::Uploader::Base.cache_dir}/#{relative_path}"
   end
 end
