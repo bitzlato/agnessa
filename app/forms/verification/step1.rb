126abc474d3f64d4a1ab12568955e3c2312c7cf2
elnikov <elnikov.s@gmail.com>
2022-06-29 10:59:01 +0300

    fix: refcator

--- app/forms/verification/step1.rb
+++ app/forms/verification/step1.rb
@@ -1,19 +1,19 @@
-class Step1Form < Verification
+class Step1Form
   include ActiveFormModel
 
-  # list all the permitted params
-  permit :first_name, :email, :password
-
-  # add validation if necessary
-  # they will be merged with base class' validation
-  validates :password, presence: true
-
-  # optional data normalization
-  def email=(email)
-    if email.present?
-      write_attribute(:email, email.downcase)
-    else
-      super
-    end
-  end
+  # # list all the permitted params
+  # permit :first_name, :email, :password
+  #
+  # # add validation if necessary
+  # # they will be merged with base class' validation
+  # validates :password, presence: true
+  #
+  # # optional data normalization
+  # def email=(email)
+  #   if email.present?
+  #     write_attribute(:email, email.downcase)
+  #   else
+  #     super
+  #   end
+  # end
 end
