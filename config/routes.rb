634e40b9b630bcb5b2a577035de7bebc06cd0e47
Danil Pismenny <danil@brandymint.ru>
2022-06-29 11:55:11 +0300

    Remove step actions

--- config/routes.rb
+++ config/routes.rb
@@ -101,14 +101,7 @@ Rails.application.routes.draw do
     scope as: :client, module: :client do
       resources :applicants, only: [], param: :encoded_external_id do
         member do
-          resources :verifications, only: [:new, :create] do
-            collection do
-              get :step1
-              get :step2
-              get :step3
-              get :step4
-            end
-          end
+          resources :verifications, only: [:new, :create]
         end
       end
 
