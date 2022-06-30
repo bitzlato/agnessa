76eb000052cd06d6184896fe9a202a6ae0e78476
elnikov <elnikov.s@gmail.com>
2022-06-30 12:28:49 +0300

    Feature/review result controller [#182593529] (#176)
    * add archived_at migration
    
    * add archiveble to router
    
    * Add shared archived_button to labels
    
    * Create and Edit labels
    
    * Only alive labels
    
    * Add seed

--- app/controllers/admin/review_result_labels_controller.rb
+++ app/controllers/admin/review_result_labels_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ReviewResultLabelsController < Admin::ApplicationController
 
   def index
-    labels = paginate ReviewResultLabel.all.order('created_at DESC')
+    labels = paginate ReviewResultLabel.all.order('archived_at DESC').order('created_at DESC')
     render locals: {labels: labels}
   end
 
@@ -9,12 +9,36 @@ class Admin::ReviewResultLabelsController < Admin::ApplicationController
     render locals: {label: label}
   end
 
+  def new
+    label = ReviewResultLabel.new
+    render locals: { label: label }
+  end
+
+
+  def create
+    country = ReviewResultLabel.create! label_params
+    redirect_back fallback_location: admin_review_result_labels_path, notice: 'Label was successfully created.'
+  rescue ActiveRecord::RecordInvalid => e
+    raise e unless e.record.is_a? ReviewResultLabel
+    render :new, locals: { label: e.record }
+  end
+
   def update
     label.update!(label_params)
     redirect_back fallback_location: admin_review_result_labels_path, notice: 'Label was successfully updated.'
   rescue ActiveRecord::RecordInvalid => e
     raise e unless e.record.is_a? Label
-    render :edit, locals: {label: label}
+    render :edit, locals: { label: label }
+  end
+
+  def restore
+    label.restore!
+    redirect_back fallback_location: admin_review_result_labels_path, notice: 'Label was successfully restored.'
+  end
+
+  def archive
+    label.archive!
+    redirect_back fallback_location: admin_review_result_labels_path, notice: 'Label was successfully archived.'
   end
 
   private
