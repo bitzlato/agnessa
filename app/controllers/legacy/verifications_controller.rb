e75414494fa906f747fe8b7e769d6720ee7e6844
Alexander Logunov <unlovedru@gmail.com>
2022-07-04 09:34:09 +0300

    [#182318585] remove legacy users export file

--- app/controllers/legacy/verifications_controller.rb
+++ app/controllers/legacy/verifications_controller.rb
@@ -1,28 +1,6 @@
 class Legacy::VerificationsController < ApplicationController
   layout 'legacy'
 
-  def index
-    # account = Account.find_by_subdomain!('bz')
-    # result = {}
-    #   account.verifications.includes(:applicant).for_export.order('verifications.created_at ASC').each do |v|
-    #     next if v.applicant.legacy_external_id.nil?
-    #
-    #     cleared_id = v.applicant.legacy_external_id.gsub('id_', '')
-    #     id = "id_#{cleared_id}"
-    #     time = v.updated_at.to_i * 1000
-    #
-    #     result[id] = {
-    #       id: id,
-    #       status: v.status.to_s == 'confirmed' ? true : false,
-    #       comment: v.public_comment.to_s.strip,
-    #       time: time
-    #     }
-    # end
-    #
-    # render json: result.values.sort_by{|v| v[:time]}.reverse
-    render json: []
-  end
-
   def legacy_show
     respond_to do |format|
       format.html
