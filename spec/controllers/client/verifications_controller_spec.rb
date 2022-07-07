ec31a2ac617daec86d22857346399928b398a117
elnikov <elnikov.s@gmail.com>
2022-07-07 17:43:01 +0300

    spec

--- spec/controllers/client/verifications_controller_spec.rb
+++ spec/controllers/client/verifications_controller_spec.rb
@@ -20,4 +20,30 @@ RSpec.describe Client::VerificationsController, type: :controller do
       expect { get :new, params: {encoded_external_id: '123'} }.to raise_exception(VerificationUrlGenerator::WrongPayload)
     end
   end
+
+  describe 'create' do
+    let(:country) { create(:country)}
+    let(:verification) { build(:verification, account: account, citizenship_country: country) }
+    it 'success create' do
+      @request.host = "#{account.subdomain}.example.com"
+      params = {
+        encoded_external_id: VerificationUrlGenerator.generate_token('123', account.secret),
+        verification: {
+          name: verification.name,
+          next_step: 5,
+          document_type: verification.document_type,
+          citizenship_country_iso_code: verification.citizenship_country_iso_code,
+          birth_date: verification.birth_date,
+          last_name: verification.last_name,
+          patronymic: verification.patronymic,
+          email: verification.email,
+          document_number: verification.document_number,
+          verification_documents_attributes:
+            account.document_types.alive.each_with_index{ |x, index| {index.to_s => {document_type_id: x.id, file: Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/image.jpg')), 'image/jpeg')}}  }
+        }
+      }
+      post :create, params: params
+      expect(response.status).to eq(200)
+    end
+  end
 end
