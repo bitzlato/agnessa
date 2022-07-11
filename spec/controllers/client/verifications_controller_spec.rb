b0c5902cd513ce01a254d0ff33d61c6da8511e76
elnikov <elnikov.s@gmail.com>
2022-07-11 09:34:36 +0300

    Spec for Verification Controller [#182341816] (#188)
    * Create Verification spec
    
    * remove
    
    * Update verifications_controller_spec.rb

--- spec/controllers/client/verifications_controller_spec.rb
+++ spec/controllers/client/verifications_controller_spec.rb
@@ -5,19 +5,45 @@ RSpec.describe Client::VerificationsController, type: :controller do
   before do
     RequestStore.store[:current_account] = account
     allow(ENV).to receive(:fetch).with('AGNESSA_BARONG_API_ROOT_URL').and_return('http://example.com')
+    allow(ENV).to receive(:fetch).with('PATH').and_return(ENV['PATH'])
     allow_any_instance_of(BarongClient).to receive(:get_p2pid_from_barong_uid).with('123').and_return(22)
+    @request.host = "#{account.subdomain}.example.com"
   end
 
   describe 'new' do
     it 'opens page with valid encoded_external_id ' do
-      @request.host = "#{account.subdomain}.example.com"
       get :new, params: {encoded_external_id: VerificationUrlGenerator.generate_token('123', account.secret)}
       expect(response.status).to eq(200)
     end
 
     it 'fails to open page with invalid encoded_external_id ' do
-      @request.host = "#{account.subdomain}.example.com"
       expect { get :new, params: {encoded_external_id: '123'} }.to raise_exception(VerificationUrlGenerator::WrongPayload)
+    end
+  end
+
+  describe 'create' do
+    let(:country) { create(:country)}
+    let(:applicant) { create(:applicant, account: account) }
+    let(:verification) do
+      build(:verification,
+            applicant: applicant,
+            citizenship_country: country)
+    end
+    let(:params) do
+      {
+        encoded_external_id: VerificationUrlGenerator.generate_token('123', account.secret),
+        verification: verification.attributes.merge(
+          verification_documents_attributes: verification.verification_documents.map do |doc|
+            doc.attributes.merge(
+              file: fixture_file_upload(File.join(Rails.root, '/spec/fixtures/image.jpg'), 'image/jpeg')
+            )
+          end
+        )
+      }
+    end
+    it 'success' do
+      post :create, params: params
+      expect(response.status).to eq(200)
     end
   end
 end
