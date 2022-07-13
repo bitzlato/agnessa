0728e127f09d33abe5979c8bf94ed38ea1bb86f8
Danil Pismenny <danil@brandymint.ru>
2022-07-13 09:44:22 +0300

    Fix locales setup

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
-I18n.available_locales = Rails.application.config.application.available_locales
-I18n.default_locale =  Rails.application.config.application.default_locale
+Rails.application.config.i18n.available_locales = Rails.application.config.application.available_locales
+Rails.application.config.i18n.default_locale = Rails.application.config.application.default_locale
+Rails.application.config.i18n.fallbacks = Rails.application.config.application.fallbacks
